0a1dada8704ab89f95ea608594c0a0801d0ecf46
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-08 14:27:44 +0200

    update components

--- space-app/src/componentes/BarraLateral/index.jsx
+++ space-app/src/componentes/BarraLateral/index.jsx
@@ -1,36 +1,33 @@
 import { styled } from "styled-components";
+import ItemNavegacao from "./ItemNavegacao";
 
 const ListaEstilizada = styled.ul`
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    width: 212px;
-`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 212px;
+`;
 
 const BarraLateral = () => {
-    return (    
-        <aside>
-            <nav>
-                <ListaEstilizada>
-                    <li>
-                        <a href="">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="">Mais vistas</a>
-                    </li>
-                    <li>
-                        <a href="">Mais curtidas</a>
-                    </li>
-                    <li>
-                        <a href="">Novas</a>
-                    </li>
-                    <li>
-                        <a href="">Surpreenda-me</a>
-                    </li>
-                </ListaEstilizada>
-            </nav>
-        </aside>
-    )
-}
+  return (
+    <aside>
+      <nav>
+        <ListaEstilizada>
+          <ItemNavegacao
+            iconeAtivo="/icones/home-ativo.png"
+            iconeInativo="/icones/home-inativo.png"
+            ativo={true}>
+            Inicio
+          </ItemNavegacao>
+          <ItemNavegacao
+            iconeAtivo="/icones/mais-vistas-ativo.png"
+            iconeInativo="/icones/mais-vistas-inativo.png">
+            Mais vistas
+          </ItemNavegacao>
+        </ListaEstilizada>
+      </nav>
+    </aside>
+  );
+};
 
 export default BarraLateral;
