d89072338d14e6f25ed9f94c61fb05389bc479a9
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-15 09:53:30 +0200

    update tags

--- space-app/src/componentes/Galeria/Tags/index.jsx
+++ space-app/src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,14 @@
+import tags from "./tags.json";
+
 const Tags = () => {
-  return <p>Tags</p>
+  return (
+    <>
+      <p>Busque por Tags:</p>
+      {tags.map((tag) => (
+        <button key={tag.id}>{tag.titulo}</button>
+      ))}
+    </>
+  );
 };
 
 export default Tags;
