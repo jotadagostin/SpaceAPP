37756fff19c7da6d1b1e29e23ca7a99cd9b859d1
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-16 15:37:38 +0200

    Update index.jsx
    tag styled component

--- space-app/src/componentes/Galeria/Tags/index.jsx
+++ space-app/src/componentes/Galeria/Tags/index.jsx
@@ -1,21 +1,49 @@
 import tags from "./tags.json";
-import { styled } from 'styled-components';
-
+import { styled } from "styled-components";
 
 const TagsContainer = styled.section`
   display: flex;
   align-items: center;
   gap: 64px;
   margin-top: 56px;
-`
+`;
+
+const TagTitulo = styled.h3`
+  color: #d9d9d9;
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Tag = styled.button`
+  font-size: 24px;
+  color: #ffffff;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 12px;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  &:hover {
+    border-color: #c98cf1;
+  }
+`;
+
+const Div = styled.div`
+  display: flex;
+  gap: 24px;
+  justify-content: end;
+`;
 
 const Tags = () => {
   return (
     <TagsContainer>
-      <p>Busque por Tags:</p>
-      {tags.map((tag) => (
-        <button key={tag.id}>{tag.titulo}</button>
-      ))}
+      <TagTitulo>Busque por tags:</TagTitulo>
+      <Div>
+        {tags.map((tag) => (
+          <Tag key={tag.id}>{tag.titulo}</Tag>
+        ))}
+      </Div>
     </TagsContainer>
   );
 };
