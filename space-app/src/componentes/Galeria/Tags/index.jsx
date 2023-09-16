2839b22269f2ed920a2931dca54073c0a68a792d
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-16 15:26:31 +0200

    Update index.jsx

--- space-app/src/componentes/Galeria/Tags/index.jsx
+++ space-app/src/componentes/Galeria/Tags/index.jsx
@@ -1,13 +1,22 @@
 import tags from "./tags.json";
+import { styled } from 'styled-components';
+
+
+const TagsContainer = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 64px;
+  margin-top: 56px;
+`
 
 const Tags = () => {
   return (
-    <>
+    <TagsContainer>
       <p>Busque por Tags:</p>
       {tags.map((tag) => (
         <button key={tag.id}>{tag.titulo}</button>
       ))}
-    </>
+    </TagsContainer>
   );
 };
 
