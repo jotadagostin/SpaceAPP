62402adeccafa4f00ed72cda02a3ea6a1b7471b9
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-17 17:36:21 +0200

    Update index.jsx

--- space-app/src/componentes/Galeria/Imagem/index.jsx
+++ space-app/src/componentes/Galeria/Imagem/index.jsx
@@ -1,7 +1,15 @@
+
+
+// https://github.com/alura-cursos/space-app/blob/aula-3/src/componentes/Galeria/Imagem/index.jsx
+// estudar esse codigo e estrutura
+
+
+
+
 const Imagem = () => {
     return(
         <figure>
-            
+
         </figure>
     )
 }
