45dc38e2afd0d6b14175c4cc2bcc61028c25f291
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-19 10:05:17 +0200

    new component
    modal

---
+++ space-app/src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,14 @@
+const ModalZomm = () => {
+  return (
+    <>
+      <dialog open>
+        <p>Exemplo do Mozilla</p>
+        <form method="dialog">
+          <button type="submit">ok</button>
+        </form>
+      </dialog>
+    </>
+  );
+};
+
+export default ModalZomm;
