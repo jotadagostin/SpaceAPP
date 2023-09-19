6a04d65ace179d29b7056382de9be5a6248d3b15
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-19 11:09:14 +0200

    Update index.jsx

--- space-app/src/componentes/ModalZoom/index.jsx
+++ space-app/src/componentes/ModalZoom/index.jsx
@@ -1,12 +1,34 @@
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem";
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
+
 const ModalZomm = ({ foto }) => {
   return (
     <>
-      <dialog open={!!foto}>
-        <p>Exemplo do Mozilla</p>
-        <form method="dialog">
-          <button type="submit">ok</button>
-        </form>
-      </dialog>
+      {foto && (
+        <>
+          <Overlay />
+          <DialogEstilizado open={!!foto}>
+            <Imagem foto={foto} expandida={true} />
+            <form method="dialog">
+              <button type="submit">ok</button>
+            </form>
+          </DialogEstilizado>
+        </>
+      )}
     </>
   );
 };
