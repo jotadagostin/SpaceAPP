940a478fb383b46b56d4313bd25242a963f52104
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-09-22 10:38:13 +0200

    favorito jsx
    protno

--- space-app/src/componentes/Galeria/Imagem/index.jsx
+++ space-app/src/componentes/Galeria/Imagem/index.jsx
@@ -43,6 +43,10 @@ const Imagem = ({
   aoZoomSolicitado,
   aoAlternarFavorito,
 }) => {
+  const iconeFavorito = foto.favorita
+    ? "/icones/favorito-ativo.png"
+    : "/icones/favorito.png";
+
   return (
     <Figure $expandida={expandida} id={`foto-${foto.id}`}>
       <img src={foto.path} alt={foto.alt} />
@@ -51,7 +55,7 @@ const Imagem = ({
         <Rodape>
           <h4>{foto.fonte}</h4>
           <BotaoIcone onClick={() => aoAlternarFavorito(foto)}>
-            <img src="/icones/favorito.png" alt="Icone de favorito" />
+            <img src={iconeFavorito} alt="Icone de favorito" />
           </BotaoIcone>
           {!expandida && (
             <BotaoIcone
